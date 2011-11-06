3b7b0fd56fcb63e0905d226e313c183433d6cd94
Daniel Princ <dan@princ.name>
2011-11-06 15:42:00 +0100

    funkcni verze

--- src/pda/lupa/MyGLSurfaceView.java
+++ src/pda/lupa/MyGLSurfaceView.java
@@ -31,23 +31,50 @@ public class MyGLSurfaceView extends GLSurfaceView  implements GLSurfaceView.Ren
     FloatBuffer cubeBuff;
     FloatBuffer texBuff;
     
-    private Square square; 
+    
+    
+    private FloatBuffer textureBuffer;	// buffer holding the texture coordinates
+    private float texture[] = {
+	// Mapping coordinates for the vertices
+	0.0f, 1.0f,		// top left	(V2)
+	0.0f, 0.0f,		// bottom left	(V1)
+	1.0f, 1.0f,		// top right	(V4)
+	1.0f, 0.0f		// bottom right	(V3)
+    };
+    
+    private FloatBuffer vertexBuffer;	// buffer holding the vertices
+    private float vertices[] = {
+	-1.0f, -1.0f,  0.0f,		// V1 - bottom left
+	-1.0f,  1.0f,  0.0f,		// V2 - top left
+	 1.0f, -1.0f,  0.0f,		// V3 - bottom right
+	 1.0f,  1.0f,  0.0f		// V4 - top right
+    };
     
     public MyGLSurfaceView(Context c, AttributeSet a) {
 	super(c, a);
 	this.context = c;
-
 	//nastaveni pruhlednosti pozadi!
 	this.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
 	this.getHolder().setFormat(PixelFormat.TRANSLUCENT);
 
 	//nastavime render
 	this.setRenderer(this);		
-
 	//prej to je na nekterych zarizeni treba
 	this.setZOrderOnTop(true);
 	
-	 this.square = new Square();
+	// predpripravime si ctverec !!
+	ByteBuffer byteBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
+	byteBuffer.order(ByteOrder.nativeOrder());
+	vertexBuffer = byteBuffer.asFloatBuffer();
+	vertexBuffer.put(vertices);
+	vertexBuffer.position(0);
+
+	byteBuffer = ByteBuffer.allocateDirect(texture.length * 4);
+	byteBuffer.order(ByteOrder.nativeOrder());
+	textureBuffer = byteBuffer.asFloatBuffer();
+	textureBuffer.put(texture);
+	textureBuffer.position(0);
+	 
     }
     
     public void setPreviewSize(Camera.Size size) {
@@ -74,6 +101,7 @@ public class MyGLSurfaceView extends GLSurfaceView  implements GLSurfaceView.Ren
 
 	//Really Nice Perspective Calculations
 	gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_NICEST);
+	
     
     }
     
@@ -90,7 +118,7 @@ public class MyGLSurfaceView extends GLSurfaceView  implements GLSurfaceView.Ren
 	//Calculate The Aspect Ratio Of The Window
 	GLU.gluPerspective(gl, 25.0f, (float)height / (float)width, 0.1f, 10.0f);
 	
-	//square.draw(gl);
+	
 
 	gl.glMatrixMode(GL10.GL_MODELVIEW); 	//Select The Modelview Matrix
 	gl.glLoadIdentity(); 
@@ -100,18 +128,44 @@ public class MyGLSurfaceView extends GLSurfaceView  implements GLSurfaceView.Ren
     public void onDrawFrame(GL10 gl) {
 	// clear Screen and Depth Buffer
 	gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+	if(cameraFrame == null) return;
 
 	// Reset the Modelview Matrix
 	gl.glLoadIdentity();
 
-	square.loadGLTexture(gl, this.context);
+	
 	// Drawing
 	gl.glTranslatef(0.0f, 0.0f, -5.0f);		// move 5 units INTO the screen
-							// is the same as moving the camera 5 units away
-	square.draw(gl);
+					
+	// is the same as moving the camera 5 units away
+	
+	
+	
+	gl.glNormal3f(0,0,1);
+	bindCameraTexture(gl);
+	// Point to our buffers
+	gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+	gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+
+	// Set the face rotation
+	gl.glFrontFace(GL10.GL_CW);
+	// set the colour for the square
+	//gl.glColor4f(0.0f, 1.0f, 0.0f, 0.5f);
+
+	// Point to our vertex buffer
+	gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+	gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
+
+	// Draw the vertices as triangle strip
+	gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
+
+	//Disable the client state before leaving
+	gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+	gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+	
+	gl.glBindTexture(GL10.GL_TEXTURE_2D, cameraTexture[0]);;
     }
  
-    Bitmap bitmap = null;
   
     /**
      * Volana pri kazdem novem framu
@@ -130,8 +184,6 @@ public class MyGLSurfaceView extends GLSurfaceView  implements GLSurfaceView.Ren
 		yuvsCounter=yuvsCounter+this.prevY;
 		bwCounter=bwCounter+this.prevY;
 	}
-	
-	
     }
     
     /**
@@ -160,6 +212,7 @@ public class MyGLSurfaceView extends GLSurfaceView  implements GLSurfaceView.Ren
 	    }
     }
     
+    /*
     FloatBuffer makeFloatBuffer(float[] arr) {
 	ByteBuffer bb = ByteBuffer.allocateDirect(arr.length*4);
 	bb.order(ByteOrder.nativeOrder());
@@ -167,104 +220,6 @@ public class MyGLSurfaceView extends GLSurfaceView  implements GLSurfaceView.Ren
 	fb.put(arr);
 	fb.position(0);
 	return fb;
-    }
-    
-    private FloatBuffer textureBuffer;	// buffer holding the texture coordinates
-    private float texture[] = {
-		    // Mapping coordinates for the vertices
-		    0.0f, 0.0f,		// top left		(V2)
-		    1.0f, 0.0f,		// bottom left	(V1)
-		    0.0f, 1.0f,		// top right	(V4)
-		    1.0f, 1.0f		// bottom right	(V3)
-    };
-    
-
-    
-    private class Square {
-	private FloatBuffer vertexBuffer;	// buffer holding the vertices
-
-	private float vertices[] = {
-			-1.0f, -1.0f,  0.0f,		// V1 - bottom left
-			-1.0f,  1.0f,  0.0f,		// V2 - top left
-			 1.0f, -1.0f,  0.0f,		// V3 - bottom right
-			 1.0f,  1.0f,  0.0f			// V4 - top right
-	};
-
-	public Square() {
-	    ByteBuffer byteBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
-	    byteBuffer.order(ByteOrder.nativeOrder());
-	    vertexBuffer = byteBuffer.asFloatBuffer();
-	    vertexBuffer.put(vertices);
-	    vertexBuffer.position(0);
-
-	    byteBuffer = ByteBuffer.allocateDirect(texture.length * 4);
-	    byteBuffer.order(ByteOrder.nativeOrder());
-	    textureBuffer = byteBuffer.asFloatBuffer();
-	    textureBuffer.put(texture);
-	    textureBuffer.position(0);
-	}
-	
-	/** The texture pointer */
-	private int[] textures = new int[1];
-
-	public void loadGLTexture(GL10 gl, Context context) {
-	    // loading texture
-	    //if(cameraFrame == null) return;
-	    
-	    bindCameraTexture(gl);
-	    if(true) return;
-	    
-	    bitmap = BitmapFactory.decodeResource(context.getResources(),R.drawable.bitmap);
-	    // generate one texture pointer
-	    gl.glGenTextures(1, textures, 0);
-	    // ...and bind it to our array
-	    gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
-
-	    // create nearest filtered texture
-	    gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_NEAREST);
-	    gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
-
-	    // Use Android GLUtils to specify a two-dimensional texture image from our bitmap
-	    GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
-	    
-	    /*gl.glTexImage2D(GL10.GL_TEXTURE_2D, 0, GL10.GL_LUMINANCE, 
-			    prevX, prevY, 0, GL10.GL_LUMINANCE, 
-			    GL10.GL_UNSIGNED_BYTE, ByteBuffer.wrap(cameraFrame));*/
-
-	    // Clean up
-	    //bitmap.recycle();
-	}
-
-
-
-	/** The draw method for the square with the GL context */
-	public void draw(GL10 gl) {
-	    if(cameraFrame == null) return;
-	    // bind the previously generated texture
-	    /// gl.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
-	    
-	    gl.glBindTexture(GL10.GL_TEXTURE_2D, cameraTexture[0]);
-
-	    // Point to our buffers
-	    gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-	    gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-
-	    // Set the face rotation
-	    gl.glFrontFace(GL10.GL_CW);
-	    // set the colour for the square
-	    //gl.glColor4f(0.0f, 1.0f, 0.0f, 0.5f);
-
-	    // Point to our vertex buffer
-	    gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
-	    gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, textureBuffer);
-
-	    // Draw the vertices as triangle strip
-	    gl.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length / 3);
-
-	    //Disable the client state before leaving
-	    gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
-	    gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
-	}
-
-    }
+    }*/
+ 
 }
